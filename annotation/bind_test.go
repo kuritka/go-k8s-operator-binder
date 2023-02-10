c56f93561c1ee9870d845d4607924ed679371faf
kuritka <kuritka@gmail.com>
2023-02-10 14:57:54 +0200

    extending UnitTests

--- annotation/bind_test.go
+++ annotation/bind_test.go
@@ -28,40 +28,132 @@ import (
 // Testee public test structure
 
 type arnold struct {
-	id      int      `annotation:"my.com/id, require=true"`
-	name    string   `annotation:"my.com/name"`
-	Surname string   `annotation:"my.com/surname, default=Schwarzenegger"`
-	Ranks   []string `annotation:"my.com/ranks"`
-	Armed   bool     `annotation:"my.com/armed,protected=true"`
+	// int value
+	id int `annotation:"controller.example.com/id, require=true"`
+	// float value
+	Budget float64
+	// string value
+	Name string `annotation:"controller.example.com/Name"`
+	// private field
+	surname string `annotation:"controller.example.com/surname, default=Schwarzenegger"`
+	// slice value
+	Ranks []string `annotation:"controller.example.com/ranks"`
+	// bool value
+	Armed bool `annotation:"controller.example.com/armed,protected=true"`
 	// nested structure
-	Credentials struct {
-		KeyID     string `annotation:"my.com/keyid, require=true"`
-		secretKey string `annotation:"my.com/secret, require=true"`
+	Inventory struct {
+		//
+		Rope string `annotation:"controller.example.com/rope, require=true"`
+		//
+		paperWithSecretKey string `annotation:"controller.example.com/secret, require=true"`
 	}
 }
 
 func (a *arnold) String() string {
-	return fmt.Sprintf("name: %s %s, ranks:%v, armed:%v, creds: [%s: %s]", a.name, a.Surname, a.Ranks, a.Armed, a.Credentials.KeyID, a.Credentials.secretKey)
+	if a.Armed {
+		return fmt.Sprintf("Machine Gun %s %s, ranks:%v, inventory: [%s, %s]", a.Name, a.surname, a.Ranks, a.Inventory.Rope, a.Inventory.paperWithSecretKey)
+	}
+	return fmt.Sprintf("%s %s in full retreat, ranks:%v, inventory: [%s, %s]", a.Name, a.surname, a.Ranks, a.Inventory.Rope, a.Inventory.paperWithSecretKey)
 }
 
 func TestBind(t *testing.T) {
-	m := map[string]string{
-		"my.com/id":      "1500",
-		"my.com/name":    "Arnold",
-		"my.com/surname": "Rimmer",
-		"my.com/ranks":   "officer1,capitan,general,major",
-		"my.com/armed":   "false",
-		"my.com/keyid":   "AKIAIOSFODNN7EXAMPLE",
-		"my.com/secret":  "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+	// arrange
+	getPredefinedInput := func() (map[string]string, *arnold) {
+		return map[string]string{
+			"controller.example.com/id":      "1500",
+			"controller.example.com/Name":    "Arnold",
+			"controller.example.com/surname": "Rimmer",
+			"controller.example.com/ranks":   "officer1,capitan,general,major",
+			"controller.example.com/armed":   "false",
+			"controller.example.com/rope":    "dipper rope",
+			"controller.example.com/secret":  "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+		}, new(arnold)
 	}
 
-	// arrange
+	tests := []struct {
+		name           string
+		expectedResult string
+		expectedError  bool
+		modifier       func(map[string]string, *arnold)
+	}{
+		{
+			name:           "private field",
+			expectedResult: "Arnold Rimmer in full retreat, ranks:[officer1 capitan general major], inventory: [dipper rope, wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY]",
+			expectedError:  false,
+			modifier:       func(m map[string]string, a *arnold) {},
+		},
+		{
+			name:           "protected field",
+			expectedResult: "Machine Gun Arnold Rimmer, ranks:[officer1 capitan general major], inventory: [dipper rope, wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY]",
+			expectedError:  false,
+			modifier: func(m map[string]string, a *arnold) {
+				a.Armed = true
+			},
+		},
+		{
+			name:          "missing required field",
+			expectedError: true,
+			modifier: func(m map[string]string, a *arnold) {
+				delete(m, "controller.example.com/id")
+			},
+		},
+		{
+			name:          "empty required field",
+			expectedError: true,
+			modifier: func(m map[string]string, a *arnold) {
+				m["controller.example.com/id"] = ""
+			},
+		},
+		{
+			name:           "empty non required field",
+			expectedError:  false,
+			expectedResult: "Arnold  in full retreat, ranks:[officer1 capitan general major], inventory: [dipper rope, wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY]",
+			modifier: func(m map[string]string, a *arnold) {
+				m["controller.example.com/surname"] = ""
+			},
+		},
+		{
+			name:          "empty map",
+			expectedError: true,
+			modifier: func(m map[string]string, a *arnold) {
+				for k := range m {
+					delete(m, k)
+				}
+			},
+		},
+	}
 
 	// act
-	tok := &arnold{Armed: true}
-	err := Bind(m, tok)
-
 	// assert
-	assert.NoError(t, err)
-	assert.Equal(t, tok.String(), "name: Arnold Rimmer, ranks:[officer1 capitan general major], armed:true, creds: [AKIAIOSFODNN7EXAMPLE: wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY]")
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			m, a := getPredefinedInput()
+			test.modifier(m, a)
+			err := Bind(m, a)
+			assert.Equal(t, test.expectedError, err != nil)
+			if !test.expectedError {
+				assert.Equal(t, test.expectedResult, a.String())
+			}
+		})
+	}
+}
+
+func TestBreaking(t *testing.T) {
+	tests := []struct {
+		name          string
+		m             map[string]string
+		s             interface{}
+		expectedError bool
+	}{
+		{name: "nil map", m: nil, s: &arnold{}, expectedError: false},
+		{name: "nil structure", m: map[string]string{}, s: nil, expectedError: true},
+	}
+	// act
+	// assert
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := Bind(test.m, test.s)
+			assert.Equal(t, err != nil, test.expectedError)
+		})
+	}
 }
