cc28c484588b7d8188ffa43f8d9fa7a5a7a3530b
kuritka <kuritka@gmail.com>
2023-02-14 16:03:26 +0200

    license update

--- env/env.go
+++ env/env.go
@@ -2,7 +2,7 @@
 package env
 
 /*
-Copyright 2023 The AbsaOSS Contributors.
+Copyright 2023 Absa Group Limited
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
